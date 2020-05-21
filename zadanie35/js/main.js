16f070e58c85f54fd17dfc7098e1e718978316f6
pmarcin98 <pmarcin98@wp.pl>
2020-05-21 12:37:09 +0200

    poprawione zadanie35

--- zadanie35/js/main.js
+++ zadanie35/js/main.js
@@ -1,7 +1,8 @@
 let firstNumber = 1;
 let secondNumber = 2;
+
 function test(firstNumber, secondNumber) {
     let suma = firstNumber + secondNumber;
     return suma;
   }
- 
+ console.log( test(firstNumber, secondNumber) );
